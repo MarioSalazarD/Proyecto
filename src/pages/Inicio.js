c7f72858bfa01681e5f61a9f1b9f7df20e9721e3
MarioSalazarD <56806942+MarioSalazarD@users.noreply.github.com>
2021-09-16 12:49:51 -0500

    Add files via upload

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/pizza.jpeg";
+import "../Estilos/Inicio.css";
 
 function Inicio() {
     return (
-        <div>
-            Hello World
+      <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+        <div className="headerContainer">
+          <h1> Mario's Pizzeria </h1>
+          <p> PIZZA PARA CUALQUIER GUSTO</p>
+          <Link to="/menu">
+            <button> HAZ TU ORDEN </button>
+          </Link>
         </div>
-    )
-}
-
-export default Inicio
+      </div>
+    );
+  }
+  
+  export default Inicio;
