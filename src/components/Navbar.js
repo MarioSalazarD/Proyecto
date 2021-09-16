0e410e09536a7c6c9a78ad124ace61b5792b722f
MarioSalazarD <56806942+MarioSalazarD@users.noreply.github.com>
2021-09-16 12:50:17 -0500

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 import '../Estilos/Navbar.css';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import Logo from '../assets/pizzaLogo.png';
 import { Link } from 'react-router-dom';
 
-
 function Navbar() {
+    const [openLinks, setOpenLinks] = useState(false);
+  
+    const toggleNavbar = () => {
+      setOpenLinks(!openLinks);
+    };
     return (
-        <div className='navbar'>
-            <div className='leftSide'>
-                <img src={Logo} />
-                <div className= 'hiddenLinks'>
-                <Link to='/'> Inicio </Link>
-                <Link to='/menu'> Menu </Link>
-                <Link to='/about'> Acerca </Link>
-                <Link to='/contact'> Contacto </Link>
-                </div>
-            </div>
-            <div className='rightSide'>
-                <Link to='/'> Inicio </Link>
-                <Link to='/menu'> Menu </Link>
-                <Link to='/about'> Acerca </Link>
-                <Link to='/contact'> Contacto </Link>
-                <button>
-                <ReorderIcon/>
-                </button>
-            </div>
+      <div className="navbar">
+        <div className="leftSide" id={openLinks ? "open" : "close"}>
+          <img src={Logo} />
+          <div className="hiddenLinks">
+            <Link to="/"> Inicio </Link>
+            <Link to="/menu"> Menu </Link>
+            <Link to="/about"> Acerca </Link>
+            <Link to="/contact"> Contacto </Link>
+          </div>
         </div>
-    )
-}
-
-export default Navbar
+        <div className="rightSide">
+          <Link to="/"> Inicio </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> Acerca </Link>
+          <Link to="/contact"> Contacto </Link>
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Navbar;
